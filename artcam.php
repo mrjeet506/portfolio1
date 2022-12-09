93a4f958aa27b7166b33bdcbfa8818bf3ff774b6
Jeet Patel <84220340+mrjeet506@users.noreply.github.com>
2022-12-09 17:30:50 +0530

    Add files via upload

---
+++ artcam.php
@@ -0,0 +1,271 @@
+<?php include 'header.html' ?>
+
+
+
+
+
+
+        <div class="stricky-header stricked-menu main-menu">
+            <div class="sticky-header__content"></div>
+            <!-- /.sticky-header__content -->
+        </div>
+        <!-- /.stricky-header -->
+
+        <!--Page Header Start-->
+        <section class="page-header">
+            <div class="page-header-bg" style="background-image: url(assets/images/slider/01.jpg)">
+            </div>
+            <div class="page-header-shape-1"><img src="assets/images/shapes/page-header-shape-1.png" alt=""></div>
+            <div class="container">
+                <div class="page-header__inner">
+                <ul class="thm-breadcrumb list-unstyled">
+                        <li><a href="index.php">Home</a></li>
+                        <li><span>/</span></li>
+                        <li>
+                            <a href="#">Onsite Training </a></li>
+                    </ul>
+                    <h2>ARTCAM</h2>
+                </div>
+            </div>
+        </section>
+        <!--Page Header End-->
+
+        <!--solid edge Start-->
+        <section class="why-choose-two">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6">
+                        <div class="why-choose-two__left">
+                            <div class="section-title text-left">
+                                <div class="section-sub-title-box">
+                                    <p class="section-sub-title">ARTCAM 2D </p>
+                                    <div class="section-title-shape-1">
+                                        <img src="assets/images/shapes/section-title-shape-1.png" alt="">
+                                    </div>
+                                    <div class="section-title-shape-2">
+                                        <img src="assets/images/shapes/section-title-shape-2.png" alt="">
+                                    </div>
+                                </div>
+                                <h2 class="section-title__title">ARTCAM 2D
+                                </h2>
+                            </div>
+<div class="row">
+    <div class="col-md-6">
+                            <ul class="list-unstyled about-one__points">
+                                <li>
+                                    <div class="icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="text">
+                                        <p>User Interface</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="text">
+                                        <p>Drawing Tools</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="text">
+                                        <p>Modify Tools in Artcam</p>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div class="icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="text">
+                                        <p> Working with Layers</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="text">
+                                        <p>Vector Editing Tools</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="text">
+                                        <p>Arry Tools</p>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div class="icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="text">
+                                        <p>Tool to Creat and Draw Vector</p>
+                                    </div>
+                                </li>
+                                </ul>
+</div>
+<div class="col-md-6">
+                                <ul class="list-unstyled about-one__points">
+                                <li>
+                                    <div class="icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="text">
+                                        <p>How to Work with Bitmaps in Artcam</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="text">
+                                        <p>What is the Model and how to work with it</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="text">
+                                        <p>Working with Text</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="text">
+                                        <p>Nesting tools</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="text">
+                                        <p>Machining tools</p>
+                                    </div>
+                                </li>
+                            </ul>
+                      
+                   
+                    </div>
+                            </div>
+                        </div>
+                    </div>
+        
+ <!--right side -->
+
+                    <div class="col-xl-6 col-lg-6">
+                        <div class="benefits-two__right">
+                            <div class="benefits-two__img">
+                                <img src="assets/images/solid/01.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+
+        </section>
+        <!--Why Choose Two End-->
+
+        <!--Benefits Two Start-->
+        <section class="benefits-two">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6">
+                        <div class="benefits-two__left">
+                            <div class="benefits-two__img">
+                                <img src="assets/images/solid/01.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6">
+                        <div class="benefits-two__right">
+                            <div class="section-title text-left">
+                                <div class="section-sub-title-box">
+                                    <p class="section-sub-title">ARTCAM 3D</p>
+                                    <div class="section-title-shape-1">
+                                        <img src="assets/images/shapes/section-title-shape-1.png" alt="">
+                                    </div>
+                                    <div class="section-title-shape-2">
+                                        <img src="assets/images/shapes/section-title-shape-2.png" alt="">
+                                    </div>
+                                </div>
+                                <h2 class="section-title__title">ARTCAM 3D
+                                </h2>
+                            </div>
+                        </div>
+
+                        <ul class="list-unstyled about-one__points">
+                            <li>
+                                <div class="icon">
+                                    <i class="fa fa-check"></i>
+                                </div>
+                                <div class="text">
+                                    <p>3D Relief Creation Tools</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <i class="fa fa-check"></i>
+                                </div>
+                                <div class="text">
+                                    <p>Relief Editing Tools</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <i class="fa fa-check"></i>
+                                </div>
+                                <div class="text">
+                                    <p>Working with Clipart</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <i class="fa fa-check"></i>
+                                </div>
+                                <div class="text">
+                                    <p>Advance Tools and Practice</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <i class="fa fa-check"></i>
+                                </div>
+                                <div class="text">
+                                    <p>Face Wizard / Texture Flow</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <i class="fa fa-check"></i>
+                                </div>
+                                <div class="text">
+                                    <p>Work on Project</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <i class="fa fa-check"></i>
+                                </div>
+                                <div class="text">
+                                    <p>Machining Tools</p>
+                                </div>
+                            </li>
+
+                    </div>
+        </section>
+
+
+
+        <?php include 'footer.html' ?>
+
+      
